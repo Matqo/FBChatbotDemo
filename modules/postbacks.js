a9f921d799abb6530f223bbf75deb7099f04ac98
shocks13 <shocks13@gmail.com>
2017-07-18 13:31:33 +0100

    added create

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -26,13 +26,12 @@ exports.q3 = (sender, values) => {
     messenger.getUserInfo(sender).then(response => {
         messenger.send({text: `Great. Your new Frame should arrive in 5-7 business days. `}, sender);
         setTimeout(function(){
+            messenger.send({text: `I've also sent an email confirmation with the return shipping label to cara@gmail.com. If there's anything else I can help you with please let me know.`}, sender);
             //Salesforce update records
             salesforce.createRecord(response, 'RMA__c').then(recordResult => {
+                messenger.send({text: `Salesforce update records`}, sender);
                 //messenger.send(formatter.formatRecords(recResult), sender);
             });
-            messenger.send({text: `I've also sent an email confirmation with the return shipping label to cara@gmail.com. If there's anything else I can help you with please let me know.`}, sender);
-            messenger.send({text: `Salesforce update records`}, sender);
-            //messenger.send(formatter.question4(response), sender);
         },1000);
     });
 };
