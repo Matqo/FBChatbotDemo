87ce47c8f4c842e61d81ff450c9d8e6beca30551
shocks13 <shocks13@gmail.com>
2017-07-18 14:54:57 +0100

    fix

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -26,12 +26,12 @@ exports.q3 = (sender, values) => {
     console.log('values: ', values);
     messenger.getUserInfo(sender).then(response => {
 
-        let theObject = nforce.createSObject(object);
+        let theObject = nforce.createSObject('RMA__c');
         theObject.set('Description__c', `Product dropped, speaker malfunctioning`);
         theObject.set('Contact__c', '003f4000002ML0v');
         theObject.set('RMA_Status__c', 'In-Process');
         theObject.set('Under_Warranty__c', true);
-        
+
         //Salesforce update records
         salesforce.createRecord(response, theObject).then(recordResult => {
             messenger.send({text: `Great. Your new Frame should arrive in 5-7 business days. `}, sender);
