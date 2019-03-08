1e3fb4ada195ec73ad0950499d9b7f7efba1054d
Martin Futas <mfutas@mfutas-ltm.internal.salesforce.com>
2019-03-08 12:15:30 +0000

    Second Commit

--- modules/formatter.js
+++ modules/formatter.js
@@ -3,216 +3,254 @@
 //https://john-paul-facebook-bot-demo.herokuapp.com/images?location.jpg
 
 let moment = require("moment"),
-    numeral = require("numeral");
+numeral = require("numeral");
 
 exports.sendButtons = response => {
-    console.log('In sendButtons: ', response);
-    let elements = [];
+console.log('In sendButtons: ', response);
+let elements = [];
 
-    elements.push({
-        "type":"postback",
-        "title":"Track my Order",
-        "payload":"exchange"
-    });
-    /*elements.push({
-        "type":"web_url",
-        "url":"https://www.messenger.com",
-        "title":"Exchange"
-    });*/
-    elements.push({
-        "type":"web_url",
-        "url":"https://www.messenger.com",
-        "title":"Complete the Look"
-    });
-    elements.push({
-        "type":"web_url",
-        "url":"https://www.messenger.com",
-        "title":"Transfer to an Agent"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "Your Options are as follows",
-                "buttons": elements
-            }
+elements.push({
+    "type":"postback",
+    "title":"Track my Order",
+    "payload":"exchange"
+});
+/*elements.push({
+    "type":"web_url",
+    "url":"https://www.messenger.com",
+    "title":"Exchange"
+});*/
+elements.push({
+    "type":"web_url",
+    "url":"https://www.messenger.com",
+    "title":"Complete the Look"
+});
+elements.push({
+    "type":"web_url",
+    "url":"https://www.messenger.com",
+    "title":"Transfer to an Agent"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "button",
+            "text": "Your Options are as follows",
+            "buttons": elements
         }
-    };
+    }
+};
 };
 
-exports.sendImage = response => {
-    console.log('In sendMap: ', response);
-    let elements = [];
+exports.sendButtons2 = response => {
+console.log('In sendButtons: ', response);
+let elements = [];
 
-    elements.push({
-        "title": "PREDATOR 19.3 FIRM GROUND BOOTS",
-        "subtitle": "£74.95 | Size: 11 | Status: Not yet Picked",
-        "image_url": "https://i.imgur.com/Liee3W0.png"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
+elements.push({
+    "type":"web_url",
+    "url":"https://www.messenger.com",
+    "title":"Change the Date"
+});
+elements.push({
+    "type":"postback",
+    "title":"Collect in Store",
+    "payload":"sendMap"
+});
+/*elements.push({
+    "type":"web_url",
+    "url":"https://www.messenger.com",
+    "title":"Exchange"
+});*/
+
+elements.push({
+    "type":"web_url",
+    "url":"https://www.messenger.com",
+    "title":"Do Nothing"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "button",
+            "text": "Your Options are as follows",
+            "buttons": elements
         }
-    };
+    }
+};
+};
+
+
+exports.sendImage = response => {
+console.log('In sendMap: ', response);
+let elements = [];
+
+elements.push({
+    "title": "PREDATOR 19.3 FIRM GROUND BOOTS",
+    "subtitle": "Price: £74.95 | Size: 11 | Status: Not yet Picked",
+    "image_url": "https://i.imgur.com/Liee3W0.png"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": elements
+        }
+    }
+};
 };
 
 exports.sendImages = response => {
-    console.log('In sendImages: ', response);
-    let elements = [];
+console.log('In sendImages: ', response);
+let elements = [];
 
-    elements.push({
-        "title": "Capri Print Silk Bodycon",
-        "subtitle":"£219.00",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress1.jpg",
-        "buttons": [
-            {
-                "type": "postback",
-                "title": "Select",
-                "payload": "selectDress,dress1"
-            }
-        ]
-    });
-    elements.push({
-        "title": "Palmyra Print Silk Bodycon",
-        "subtitle":"£219.00",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress2.jpg",
-        "buttons": [
-            {
-                "type": "postback",
-                "title": "Select",
-                "payload": "selectDress,dress2"
-            }
-        ]
-    });
-    elements.push({
-        "title": "Peony Print Pleat Detail Dress",
-        "subtitle":"£195.00",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress3.jpg",
-        "buttons": [
-            {
-                "type": "postback",
-                "title": "Select",
-                "payload": "selectDress,dress3"
-            }
-        ]
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
+elements.push({
+    "title": "Capri Print Silk Bodycon",
+    "subtitle":"£219.00",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress1.jpg",
+    "buttons": [
+        {
+            "type": "postback",
+            "title": "Select",
+            "payload": "selectDress,dress1"
         }
-    };
+    ]
+});
+elements.push({
+    "title": "Palmyra Print Silk Bodycon",
+    "subtitle":"£219.00",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress2.jpg",
+    "buttons": [
+        {
+            "type": "postback",
+            "title": "Select",
+            "payload": "selectDress,dress2"
+        }
+    ]
+});
+elements.push({
+    "title": "Peony Print Pleat Detail Dress",
+    "subtitle":"£195.00",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress3.jpg",
+    "buttons": [
+        {
+            "type": "postback",
+            "title": "Select",
+            "payload": "selectDress,dress3"
+        }
+    ]
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": elements
+        }
+    }
+};
 };
 
 exports.dress1 = response => {
-    console.log('In dress1: ', response);
-    let elements = [];
+console.log('In dress1: ', response);
+let elements = [];
 
-    elements.push({
-        "title": "Capri Print Silk Bodycon",
-        "subtitle":"£219.00",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress1.jpg"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
+elements.push({
+    "title": "Capri Print Silk Bodycon",
+    "subtitle":"£219.00",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress1.jpg"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": elements
         }
-    };
+    }
+};
 };
 
 exports.dress2 = response => {
-    console.log('In dress2: ', response);
-    let elements = [];
+console.log('In dress2: ', response);
+let elements = [];
 
-    elements.push({
-        "title": "Palmyra Print Silk Bodycon",
-        "subtitle":"£219.00",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress2.jpg"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
+elements.push({
+    "title": "Palmyra Print Silk Bodycon",
+    "subtitle":"£219.00",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress2.jpg"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": elements
         }
-    };
+    }
+};
 };
 
 exports.dress3 = response => {
-    console.log('In dress3: ', response);
-    let elements = [];
+console.log('In dress3: ', response);
+let elements = [];
 
-    elements.push({
-        "title": "Peony Print Pleat Detail Dress",
-        "subtitle":"£195.00",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress3.jpg"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
+elements.push({
+    "title": "Peony Print Pleat Detail Dress",
+    "subtitle":"£195.00",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?dress3.jpg"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": elements
         }
-    };
+    }
+};
 };
 
 exports.sendMap = response => {
-    console.log('In sendMap: ', response);
-    let elements = [];
+console.log('In sendMap: ', response);
+let elements = [];
 
-    elements.push({
-        "title": "Store Location",
-        "image_url": "https://phil-salesforce-bot.herokuapp.com/images?map.png"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
+elements.push({
+    "title": "Store Location",
+    "image_url": "https://phil-salesforce-bot.herokuapp.com/images?map.png"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": elements
         }
-    };
+    }
+};
 };
 
 exports.sendQuestion = response => {
-    console.log('In sendQuestion: ', response);
-    let elements = [];
+console.log('In sendQuestion: ', response);
+let elements = [];
 
-    elements.push({
-        "type":"postback",
-        "title":"Yes",
-        "payload":"yes"
-    });
-    elements.push({
-        "type":"postback",
-        "title":"No",
-        "payload":"no"
-    });
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "Is this the Store you wish to pick up?",
-                "buttons": elements
-            }
+elements.push({
+    "type":"postback",
+    "title":"Yes",
+    "payload":"yes"
+});
+elements.push({
+    "type":"postback",
+    "title":"No",
+    "payload":"no"
+});
+return {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "button",
+            "text": "Is this the Store you wish to pick up?",
+            "buttons": elements
         }
-    };
+    }
+};
 };
